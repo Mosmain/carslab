e5d84891b5560788927ff222fa7d3d4570a5012d
MosmainDev <mosmaindev@gmail.com>
2021-11-20 08:39:50 +0700

    fix modal

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ $tblName = 'cars';
 	<!-- Modal -->
 	<div class="modal fade" tabindex="-1" role="dialog" id="Modal">
 	  <div class="modal-dialog modal-dialog-centered" role="document">
-	    <div class="modal-content shadow">
+	    <div class="modal-content shadow dark add-new">
 	      <div class="modal-header">
 	        <h5 class="modal-title">Добавить автомобиль</h5>
 	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
--- modal.php
+++ modal.php
@@ -1,7 +1,7 @@
 <!-- Modal Edit-->
 <div class="modal fade" id="editModal<?=$cars['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content shadow">
+    <div class="modal-content shadow dark">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Редактировать запись № <?=$cars['id'] ?></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -42,7 +42,7 @@
 <!-- DELETE MODAL -->
 <div class="modal fade" id="deleteModal<?=$cars['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content shadow">
+    <div class="modal-content shadow dark">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Удалить запись № <?=$cars['id'] ?></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
